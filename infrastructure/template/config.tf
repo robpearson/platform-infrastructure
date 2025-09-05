55be0e851fbd4fe0ef55f3bd6773881d769f9263
Rob Pearson <rob.pearson@me.com>
2025-09-05 20:01:45 +1000

    Removed backend file and merged into config.

--- infrastructure/template/config.tf
+++ infrastructure/template/config.tf
@@ -1,10 +1,18 @@
 terraform {
+  backend "s3" {
+  }
+}
+
+terraform {
   required_providers {
-    octopusdeploy = {
-      source = "OctopusDeploy/octopusdeploy", version = "1.0.1"
+    octopusdeploy = { 
+      source = "OctopusDeploy/octopusdeploy", version = "1.3.3"
     }
   }
 }
 
 provider "octopusdeploy" {
+  address  = var.octopus_server
+  api_key  = var.octopus_apikey
+  space_id = var.octopus_space_id
 }
